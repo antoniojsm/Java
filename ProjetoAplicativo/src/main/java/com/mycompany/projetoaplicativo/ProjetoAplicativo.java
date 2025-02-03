90ca4568b6608882fd243cf1fadeb7c1742b15d5
Antonio José dos santos menezes <antoniojoseyt341@gmail.com>
2025-02-03 11:28:23 -0300

    primeiro commit aplicativo_teste

---
+++ ProjetoAplicativo/src/main/java/com/mycompany/projetoaplicativo/ProjetoAplicativo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoaplicativo;
+
+/**
+ *
+ * @author anton
+ */
+public class ProjetoAplicativo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
