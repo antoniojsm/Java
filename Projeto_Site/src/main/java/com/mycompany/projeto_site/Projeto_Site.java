c94e5bbd247b604bd8221d1a460c7d6ebf293c60
Antonio José dos santos menezes <antoniojoseyt341@gmail.com>
2025-02-03 13:37:07 -0300

    1 commit projeto_site exemplo

---
+++ Projeto_Site/src/main/java/com/mycompany/projeto_site/Projeto_Site.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_site;
+
+/**
+ *
+ * @author anton
+ */
+public class Projeto_Site {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
